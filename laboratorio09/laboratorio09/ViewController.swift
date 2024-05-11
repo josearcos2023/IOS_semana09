0bfdabc8ff849bc91e1541013e34a0e165008dc1
josearcos2023 <130238630+josearcos2023@users.noreply.github.com>
2024-05-11 14:52:47 -0500

    Add files via upload

---
+++ laboratorio09/laboratorio09/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  laboratorio09
+//
+//  Created by Mac13 on 11/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btnToSecondView: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+}
+
